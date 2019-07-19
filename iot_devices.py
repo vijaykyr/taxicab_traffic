71c69e22e677a37f56cad554d00a68cbce9b8b6f
Vijay Reddy <reddyv@google.com>
2019-07-19 17:13:56 -0400

    tested

--- iot_devices.py
+++ iot_devices.py
@@ -5,24 +5,29 @@ import argparse
 import datetime
 from google.cloud import pubsub
 
+"""
+Send sensor data to Cloud Pub/Sub in small groups, simulating real-time behavior
+"""
 TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
-TOPIC = 'sandiego'
+DEFAULT_TOPIC = 'taxi_rides'
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser(description='Send sensor data to Cloud Pub/Sub in small groups, simulating real-time behavior')
-   parser.add_argument('--project', help='Example: --project $DEVSHELL_PROJECT_ID', required=True)
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--project', help='Google Cloud Project ID', required=True)
+   parser.add_argument('--topic', help='Pub/Sub Topic, will be created if doesn\'t exist',
+                      default=DEFAULT_TOPIC)
    args = parser.parse_args()
 
    # create Pub/Sub notification topic
    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
    publisher = pubsub.PublisherClient()
-   topic_name = publisher.topic_path(args.project,TOPIC)
+   topic_name = publisher.topic_path(args.project,args.topic)
    try:
       publisher.get_topic(topic_name)
-      logging.info('Reusing pub/sub topic {}'.format(TOPIC))
+      logging.info('Reusing pub/sub topic {}'.format(args.topic))
    except:
       publisher.create_topic(topic_name)
-      logging.info('Creating pub/sub topic {}'.format(TOPIC))
+      logging.info('Creating pub/sub topic {}'.format(args.topic))
 
    while True:
         publisher.publish(topic_name,'taxi_ride')
