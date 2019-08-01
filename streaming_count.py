9f0c2ea40a114125728d4fb3c961095271380268
Vijay Reddy <you@example.com>
2019-08-01 14:26:14 +0000

    refactored

--- streaming_count.py
+++ streaming_count.py
@@ -34,53 +34,45 @@ def run(argv=None):
   """Build and run the pipeline."""
   parser = argparse.ArgumentParser()
   
-
   parser.add_argument(
       '--project',
       help=('Google Cloud Project ID'),
       required=True)  
   parser.add_argument(
       '--input_topic',
-      help=('Google Cloug PubSub topic name '),
+      help=('Google Cloud PubSub topic name '),
       required=True)
 
   known_args, pipeline_args = parser.parse_known_args(argv)
   print('Pipeline args: {}'.format(pipeline_args))
   print('Known args: {}'.format(known_args))
 
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
   pipeline_options = PipelineOptions(
     pipeline_args.append('--project={}'.format(known_args.project)))
   pipeline_options.view_as(SetupOptions).save_main_session = True
   pipeline_options.view_as(StandardOptions).streaming = True
+
   p = beam.Pipeline(options=pipeline_options)
-
-  # Read from PubSub into a PCollection.
+    
   TOPIC = 'projects/{}/topics/{}'.format(known_args.project,known_args.input_topic)
-  messages = p | beam.io.ReadFromPubSub(topic=TOPIC).with_output_types(bytes)
+  table_spec = '{}:taxifare.traffic_realtime'.format(known_args.project) # table needs to exist
 
-  windows = messages | beam.WindowInto(window.FixedWindows(size=15))
-
-  count = windows | 'count' >> beam.CombineGlobally(CountFn()).without_defaults()
-
-  def to_dict(count):
+  def to_bq_format(count):
+    """BigQuery writer requires rows to be stored as python dictionary"""
     return {'trips_last_5min':count,'time':datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
 
-  count_dict = count | 'to_dict' >> beam.Map(to_dict)
-
-  # dataset_id.table_id (table needs to exist)
-  table_spec = '{}:taxifare.traffic_realtime'.format(known_args.project)
-  
-  count_dict | 'write_bq' >> beam.io.WriteToBigQuery(
-    table_spec,
-    write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND, #WRITE_TRUNCATE not supported for streaming
-    create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER) #CREATE_IF_NEEDED didn't work, kept sleeping after create
-
+  pipeline = (p
+       | 'read_from_pubusub' >> beam.io.ReadFromPubSub(topic=TOPIC).with_output_types(bytes)
+       | 'window' >> beam.WindowInto(window.SlidingWindows(size=300,period=15))
+       | 'count' >> beam.CombineGlobally(CountFn()).without_defaults()
+       | 'format_for_bq' >> beam.Map(to_bq_format)
+       | 'write_to_bq' >> beam.io.WriteToBigQuery(
+            table_spec,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND, #WRITE_TRUNCATE not supported for streaming
+            create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER))
 
   result = p.run()
   #result.wait_until_finish() #only do this if running with DirectRunner
-
 
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
